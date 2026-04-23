8370a680a671aa495bf3b72607d3a03ae5b655f8
SamaraSilvia81 <samarasilvia.dev@gmail.com>
2026-04-23 16:27:32 -0300

    feat: Add new version with framework astro

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,8 @@
+
+export default new Map([
+["src/content/aulas/00-apresentacao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Faulas%2F00-apresentacao.mdx&astroContentModuleFlag=true")],
+["src/content/aulas/01-metodologia-agil.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Faulas%2F01-metodologia-agil.mdx&astroContentModuleFlag=true")],
+["src/content/aulas/02-como-web-funciona.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Faulas%2F02-como-web-funciona.mdx&astroContentModuleFlag=true")],
+["src/content/aulas/03-intro-html.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Faulas%2F03-intro-html.mdx&astroContentModuleFlag=true")],
+["src/content/aulas/04-css-fundamentos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Faulas%2F04-css-fundamentos.mdx&astroContentModuleFlag=true")]]);
+		
